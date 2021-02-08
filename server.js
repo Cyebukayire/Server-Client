3ff9d59a37c0953eec3ad52d24be5d610ff71da3
Cyebukayire <peacecyebukayire@gmail.com>
2021-02-08 20:21:50 +0200

    New Updates

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const http = require('http');
-const server = http.createServer((req, res)=>{
-    console.log("Request made");
+const fs = require('fs');
+// const file=require('./blogs/index.html');
+
+const port1=3000;
+
+const server = http.createServer((req,res) => {
+    console.log(req.url, req.method);
+
+    //Set header content type 
+    // res.setHeader('Content-Type', 'text/plain'); //Ignores html tags
+    res.setHeader('Content-Type', 'text/plain');
+    // res.write('<h1>Ninjas!</h1>');
+    // res.write(`<p>Hello, Ninjas!</p>`);
+    // res.write(`<p>Hello again, Ninjas!</p>`);
+
+    //Send an html file
+    fs.readFile(`./blogs/index.txt`, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
 });
-server.listen(3000, 'localhost', ()=>{
-    console.log("Listening for requests on port 3000");
+
+server.listen(port1, 'localhost', () => {
+    console.log(`Listening for requests on port ${port1}`);
 })
